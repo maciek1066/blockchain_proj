4c1e2c7e1dabe16dc08009d63f1b21bec272b3ce
maciek1066 <maciek1066@gmail.com>
2018-05-26 20:33:18 +0200

    models, views transactions, addresses

--- address_info/models.py
+++ address_info/models.py
@@ -3,14 +3,15 @@ from django.db import models
 
 
 class Transaction(models.Model):
-    transaction_id = models.CharField(max_length=54)
-    tx_json = JSONField()
+    transaction_id = models.CharField(max_length=256)
+    tx_inputs = JSONField()
+    tx_outs = JSONField()
     timestamp = models.IntegerField(null=True)
 
 
 class Address(models.Model):
     address = models.CharField(max_length=35)
-    hash = models.CharField(max_length=54)
+    hash160 = models.CharField(max_length=54)
     no_transactions = models.IntegerField()
     received = models.BigIntegerField()
     sent = models.BigIntegerField()
--- address_info/views.py
+++ address_info/views.py
@@ -24,13 +24,38 @@ class AddressInfoView(View):
             address = form.cleaned_data['address']
             if Address.objects.filter(
                     address__contains=address).exists():
-                return HttpResponse("Nice")
+                return HttpResponse("Address already exists in db")
             else:
                 response = requests.get(
                     "https://blockchain.info/rawaddr/{}".format(address))
                 data = response.json()
-                txs_data = data["txs"]
-                return HttpResponse("{}".format(transactions))
+                #  creating account
+                address = data["address"]
+                hash160 = data["hash160"]
+                no_transactions = data["n_tx"]
+                received = data["total_received"]
+                sent = data["total_sent"]
+                balance = data["final_balance"]
+                Address.objects.create(
+                    address=address,
+                    hash160=hash160,
+                    no_transactions=no_transactions,
+                    received=received,
+                    sent=sent,
+                    balance=balance,
+                )
+                # transactions many to many relation
+                for i in range(len(data["txs"])):
+                    new_trx = Transaction.objects.create(
+                        transaction_id=data["txs"][i]["hash"],
+                        tx_inputs=data["txs"][i]["inputs"],
+                        tx_outs=data["txs"][i]["out"],
+                        timestamp=data["txs"][i]["time"]
+                    )
+                    addr = Address.objects.get(address=data["address"])
+                    addr.transactions.add(new_trx)
+                    addr.save()
+                return HttpResponse("ziała")
 
 
 
